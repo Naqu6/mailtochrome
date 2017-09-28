fad37f918083084244fccac52fac0f2e4aab37f5
Ryan Ehrlich <Ryansehrlich@gmail.com>
2017-09-28 15:30:44 -0700

    Added auto-copying

--- background.js
+++ background.js
@@ -2,10 +2,27 @@ document.querySelectorAll("a").forEach((item, _) => {
 	var attrib = item.getAttribute("href");
 	if (attrib && attrib.indexOf("mailto:") != -1) {
 
-		item.setAttribute("href", "#");
+		item.setAttribute("href", "");
+		item.setAttribute("target", "")
+
 		var email = attrib.substring(7);
+        
         if (item.innerText.indexOf(email) == -1) {
         	item.innerText += " " + email;
         }
+
+       	item.addEventListener("click", (event) => {
+       		event.preventDefault()
+
+       		input = document.createElement('input');
+			document.body.appendChild(input)
+			
+			input.value = email
+			input.select();
+			
+			document.execCommand('copy', false);
+			
+			input.remove();
+       });
     }
 });
