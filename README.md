532f31d26c60ffe7b516ebbc8aaf5d46a527a158
Ryan Ehrlich <Ryansehrlich@gmail.com>
2017-09-28 15:12:02 -0700

    Inital Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mailto: the Chrome Extension that removes "Mailto" Links
+
+Some websites use the "Mailto:" tag when listing emails. This extension gets rid of those tags.
---
+++ background.js
@@ -0,0 +1,11 @@
+document.querySelectorAll("a").forEach((item, _) => {
+	var attrib = item.getAttribute("href");
+	if (attrib && attrib.indexOf("mailto:") != -1) {
+
+		item.setAttribute("href", "#");
+		var email = attrib.substring(7);
+        if (item.innerText.indexOf(email) == -1) {
+        	item.innerText += email;
+        }
+    }
+});
